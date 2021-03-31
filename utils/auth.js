8398217b194179b9c8608ccfab07eca12f22034a
duynguyen <duynguyen@meu-solutions.com>
2021-03-31 20:41:15 +0700

    remove log conosle

--- utils/auth.js
+++ utils/auth.js
@@ -2,11 +2,9 @@ const jwt = require("jsonwebtoken")
 
 const auth = (req, res, next) => {
     let authHeader = req.headers.authorization
-    // console.log(authHeader)
 
     if (authHeader) {
         const token = authHeader.split(' ')[1];
-        // console.log(token)
 
         jwt.verify(token, "SOA", (err, user) => {
             if (err) {
